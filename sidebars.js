17a42b0597815a70ccb214c2c5e0c14ef3cb485b
danielxji6 <danielxji6@gmail.com>
2025-02-06 16:10:41 -0800

    Update new API URL

--- sidebars.js
+++ sidebars.js
@@ -1,13 +1,13 @@
 module.exports = {
   someSidebar: {
-    'Getting Started': ['introduction','available-apis', 'salesforce-vs-propel', 'authentication', 
+    'Getting Started': ['introduction','available-apis', 'salesforce-vs-propel', 'authentication',
       { 'label': 'Object Model',
         'type' : 'category',
         collapsed: false,
         'items': ['object-model-core-objects','object-model-attachments']
       }
     ],
-    'Integration Patterns': 
+    'Integration Patterns':
     [
       {
       'label': 'Event-based Messaging',
@@ -17,13 +17,13 @@ module.exports = {
       }
     ],
     'Use Cases': ['erp','cad'],
-    'Platform Extensibility': 
-    [ 
+    'Platform Extensibility':
+    [
       {
         'label': 'Workspace',
         'type' : 'category',
         collapsed: false,
-        'items': ['soql-adapters', { 
+        'items': ['soql-adapters', {
                     'label':'Apex Adapters',
                     'type' :'category',
                     'items': ['apex-adapter-how-to']
@@ -32,18 +32,16 @@ module.exports = {
       'apex-invocables'
     ],
     'API Reference': [
-      // Old REST API: https://api-docs.propelplm.com
       {
         type : 'link',
         label: 'Propel REST API', // The label that should be displayed (string).
-        href : 'https://documenter.getpostman.com/view/18799091/2s93CSnVUm' // The target URL (string).
+        href : 'https://developer.propelsoftware.com' // The target URL (string).
       },
-      // Old Import API: https://documenter.getpostman.com/view/6385615/SVfKyr1m?version=latest
       {
         type : 'link',
         label: 'Propel Import API',
-        href : 'https://documenter.getpostman.com/view/18799091/2s93CSnVUm#2c9b1d3b-dc05-497c-a304-4051540242eb'
-      }  
+        href : 'https://developer.propelsoftware.com#d74bd29d-fee2-4457-82c1-c2dc17193bbe'
+      }
     ]
   },
 };
